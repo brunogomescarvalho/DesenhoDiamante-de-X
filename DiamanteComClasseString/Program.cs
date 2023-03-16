a2274c9c8d422d34223da63f1b4f9d8d3613c523
brunogomescarvalho <7bruno8@gmail.com>
2023-03-16 18:06:51 -0300

    first commit

---
+++ DiamanteComClasseString/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+Console.WriteLine("--- Pirâmide Diamante ---");
+
+var x = 0;
+do
+{
+    System.Console.WriteLine("Informe o valor de X"); 
+    x = int.Parse(Console.ReadLine()!); 
+
+    if (x % 2 == 0)
+    {
+        Console.Clear();
+        System.Console.WriteLine("O valor de X precisa ser ímpar\nTecle para continuar");
+        Console.ReadKey();
+        Console.Clear();
+    }
+
+} while (x % 2 == 0);
+
+var cursor = x + 2;
+var metade = (x - 1) / 2;
+var inf = (x - 2);
+var sup = 1;
+
+Console.CursorLeft = (cursor);
+
+for (int i = 0; i <= metade; i++)
+{
+    string superior = new String('X', sup);
+    System.Console.WriteLine(superior);
+    Console.CursorLeft = cursor -= 1;
+    sup += 2;
+}
+
+Console.CursorLeft = cursor += 2;
+
+for (int i = 0; i < metade; i++)
+{
+    string inferior = new String('X', inf);
+    System.Console.WriteLine(inferior);
+    Console.CursorLeft = cursor += 1;
+    inf -= 2;
+}
+
---
+++ DiamanteComForEIf/Program.cs
@@ -0,0 +1,77 @@
+﻿Console.WriteLine("--- Pirâmide Diamante ---");
+int tamanho = 0;
+do
+{
+    System.Console.WriteLine("Informe o valor de X");
+    tamanho = int.Parse(Console.ReadLine()!);
+
+    if (tamanho % 2 == 0)
+    {
+        Console.Clear();
+        System.Console.WriteLine("O valor de X precisa ser ímpar\nTecle para continuar");
+        Console.ReadKey();
+        Console.Clear();
+    }
+
+} while (tamanho % 2 == 0);
+
+int meio = (tamanho - 1) / 2;
+int qtdX = 1;
+int qtdBranco = meio;
+
+//superior
+
+for (int i = 0; i <= meio - 1; i++)
+{
+    for (int h = 0; h <= qtdBranco; h++)
+    {
+        System.Console.Write(" ");
+    }
+    for (int h = 0; h < qtdX; h++)
+    {
+        System.Console.Write("X");
+    }
+
+    Console.WriteLine();
+    qtdBranco--;
+    qtdX += 2;
+}
+
+meio = 1;
+
+// linha central
+
+for (int j = 0; j < meio; j++)
+{
+    System.Console.Write(" ");
+}
+for (int i = 0; i < tamanho; i++)
+{
+    System.Console.Write("X");
+}
+
+Console.WriteLine();
+
+
+// inferior
+
+qtdX = tamanho - 2;
+meio = (tamanho - 1) / 2;
+qtdBranco = 2;
+
+for (int i = 0; i < meio; i++)
+{
+    for (int h = 0; h < qtdBranco; h++)
+    {
+        System.Console.Write(" ");
+    }
+    for (int h = 0; h < qtdX; h++)
+    {
+        System.Console.Write("X");
+    }
+
+    Console.WriteLine();
+    qtdBranco++;
+    qtdX -= 2;
+}
+
